d30e92a1827f1e7cb9eec9bd55c169aeff41cd41
aldghost <52725988+aldghost@users.noreply.github.com>
2021-02-28 00:11:31 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Tugas2 Aldin - 1402019011</title>
+</head>
+
+<body>
+    <div class="header">
+        <nav class="navbar navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand">Aldin</a>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-light" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+
+    <div class="middle">
+        <div class="middle-item">
+            <nav class="nav flex-column">
+                <a class="nav-link active" aria-current="page" href="index.html">My Project</a>
+                <a class="nav-link" href="#" id="now">About Me</a>
+                <a class="nav-link" href="ask.html">Ask Me!</a>
+            </nav>
+        </div>
+        <div class="middle-item">
+            <div class="container">
+                <h2>About Me</h2>
+                <hr>
+                <figure class="figure">
+                    <img src="elon.jpg" class="figure-img img-fluid rounded" alt="...">
+                    <figcaption class="figure-caption">Lorem ipsum dolor.</figcaption>
+                </figure>
+
+                <div class="container">
+                    <h3>Hi! I am Aldin Gans</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                    <h3>Skills</h3>
+                    <ul class="list-group">
+                        <li class="list-group-item">Python</li>
+                        <li class="list-group-item">Java</li>
+                        <li class="list-group-item">Web</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <ul class="nav justify-content-end bg-primary">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Github</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Instagram</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Linkedin</a>
+            </li>
+
+        </ul>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
